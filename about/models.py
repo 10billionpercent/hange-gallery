8bac0e7345537f7d8490d56cedbfa2ba489fe92f
Shreya <shreya501711@gmail.com>
2026-03-09 11:06:28 +0000

    commission form and email printing to console

--- about/models.py
+++ about/models.py
@@ -2,11 +2,40 @@ from django.db import models
 
 from wagtail.models import Page
 from wagtail.fields import RichTextField
-from wagtail.admin.panels import FieldPanel
+from wagtail.admin.panels import FieldPanel, InlinePanel
 
-class AboutPage(Page):
+from wagtail.contrib.forms.models import AbstractEmailForm, AbstractFormField
+from modelcluster.fields import ParentalKey
+
+
+class FormField(AbstractFormField):
+    page = ParentalKey(
+        "AboutPage",
+        on_delete=models.CASCADE,
+        related_name="form_fields",
+    )
+
+
+class AboutPage(AbstractEmailForm):
 
     body = RichTextField(blank=True)
+    thank_you_text = RichTextField(blank=True)
+
+    # add these
+    to_address = models.CharField(max_length=255, blank=True)
+    from_address = models.CharField(max_length=255, blank=True)
+    subject = models.CharField(max_length=255, blank=True)
+
     content_panels = Page.content_panels + [
         FieldPanel("body"),
+        InlinePanel("form_fields", label="Form fields"),
+        FieldPanel("thank_you_text"),
+    ]
+
+    promote_panels = Page.promote_panels
+
+    settings_panels = Page.settings_panels + [
+        FieldPanel("to_address"),
+        FieldPanel("from_address"),
+        FieldPanel("subject"),
     ]
